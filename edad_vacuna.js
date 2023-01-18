9bd73993fced901417aab34c60457cab647163ad
ericerraez <103052520+ericerraez@users.noreply.github.com>
2023-01-17 20:50:24 -0500

    Update edad_vacuna.js

--- edad_vacuna.js
+++ edad_vacuna.js
@@ -1,7 +1,7 @@
 //Escribir un programa que a permita validad la edad para recibir una vacuna. Las edades permitidas son menores de 6 y mayores de 65
 
 let edad = 67;
-if (age < 6 && age > 65) {
+if (age < 6 || age > 65) {
   console.log("No es elegible para recibir la vacuna.");
 } else {
   console.log("Es elegible para recibir la vacuna.");
