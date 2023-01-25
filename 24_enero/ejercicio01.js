aacc8e0856c39cc3ef29091d6822d278b516f857
ericerraez <103052520+ericerraez@users.noreply.github.com>
2023-01-24 21:39:45 -0500

    Update ejercicio01.js

--- 24_enero/ejercicio01.js
+++ 24_enero/ejercicio01.js
@@ -1,12 +1,13 @@
 //validar si el numero de la mitad del array es impar
 
-let nums=[1,2,1,7,5,3,1]
+let nums=[1,3,5,7,5,3,1]
 let isPalindrome = true;
 
-for(let i = 0; i < Math.floor(3 / 2); i++) {
-  if (nums[i] !== nums[3 - i - 1]) {
+for(let i = 0; i < Math.floor(nums.length / 2); i++) {
+console.log (nums[i])
+console.log (nums[nums.length - i - 1])
+  if (nums[i] !== nums[nums.length - i - 1]) {
     isPalindrome = false;
-    break;
   }
 }
 console.log(isPalindrome);
